42e72f128a9fba82c270aa1c8d98412bd0ae055b
Chetan Sarnad <csarnad@csarnad-a01.vmware.com>
2020-07-10 19:11:08 +0530

    Priority queue description

---
+++ src/HeapsAndPriorityQueue/BasicImplementations/PriorityQueue.java
@@ -0,0 +1,17 @@
+package HeapsAndPriorityQueue.BasicImplementations;
+
+/*
+
+Priority queue is queue, where you add and remove elements similar to queue
+But it is not FIFO.
+
+Here the number is removed is as per priority.
+Higest priority or least priority should be removed first.
+
+So priority queues can be implemented using Heap.
+Since using Min heap or Max heap based on the requirement, we can implement priorityQueue.
+Using heap we can achieve insertion and deletion in log(n) time.
+
+ */
+public class PriorityQueue {
+}
